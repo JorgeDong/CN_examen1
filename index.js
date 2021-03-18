a642951780496fd3c06f8e202b014475ca8d3927
Jorge Dong <is714046@iteso.mx>
2021-03-17 22:30:18 -0600

    Cambio de enfoque

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const cors = require('cors')
 var util = require('util');
 var exec = require('child_process').exec;
+const fs = require('fs');
 
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
 require('dotenv').config();
 
@@ -26,34 +29,52 @@ app.use(cors(corsOptions))
 // indicamos que usaremos un router
 app.get('/', function (req, res) {
 
-var command = ''
+res.send(process.env.DB_HOST);
 
-child = exec(command, function(error, stdout, stderr){
-	console.log('stdout: ' + stdout);
-	console.log('stderr: ' + stderr);
+});
 
 
-	res.send(stdout);
-
-	if(error !== null) {
-	    console.log('exec error: ' + error);
-	}
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: '2017-09-21',
+  authenticator: new IamAuthenticator({
+    apikey: process.env.API_KEY,
+  }),
+  serviceUrl: process.env.URL_TEXT_ANALYZER,
 });
 
 
-});
+
 
 
 // indicamos que usaremos un router
 app.get('/env', function (req, res) {
 
+const text = 'Team, I know that times are tough! Product '
+  + 'sales have been disappointing for the past three '
+  + 'quarters. We have a competitive product, but we '
+  + 'need to do a better job of selling it!';
+
+const toneParams = {
+  toneInput: { 'text': text },
+  contentType: 'application/json',
+};
+
+toneAnalyzer.tone(toneParams)
+  .then(toneAnalysis => {
+    console.log(JSON.stringify(toneAnalysis, null, 2));
+
+    //res.send(JSON.stringify(toneAnalysis, null, 2));
+    res.send(toneAnalysis.result);
+
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+
+	
 
 
-	res.send(process.env.DB_HOST);
-
-
-});
-
+  });
 
 
 
