7c04a8ada39be15a6e8f74f6d8d3bc9bfac260d1
Jorge Dong <is714046@iteso.mx>
2021-03-18 00:40:39 -0600

    Renderizar con node

--- index.js
+++ index.js
@@ -23,4 +23,21 @@ const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
 app.use(cors(corsOptions))
 
 
+let http = require('http');
+
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+
 http.createServer(handleRequest).listen(8000);
